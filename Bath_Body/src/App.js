eead253a4ab998bfb4711526140a3afea0c5ce3f
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-07-20 20:31:01 +0530

    day-2

--- Bath_Body/src/App.js
+++ Bath_Body/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+
+import Login from './bharat_page/Login.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div >
+   <Login/>
     </div>
   );
 }
--- Bath_Body/src/index.js
+++ Bath_Body/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'; 
+import { ChakraProvider } from '@chakra-ui/react';
+import {BrowserRouter} from "react-router-dom"
+import { Provider } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <BrowserRouter>
+  <BrowserRouter>
+  <ChakraProvider>
+
     <App />
-  // </BrowserRouter>
+  </ChakraProvider>
+</BrowserRouter>
 
 );
 
