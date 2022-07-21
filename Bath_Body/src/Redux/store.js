1331e8fdd5d64f128480226873d4df034f6777e2
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-07-21 18:09:10 +0530

    issue Resolve

--- Bath_Body/src/Redux/store.js
+++ Bath_Body/src/Redux/store.js
@@ -1,5 +1,10 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore,combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import { ArfReducer } from "./ArfReducer/reducer";
+
+const reducer=combineReducers({
+    arfreducer:ArfReducer
+})
 
 
-
-export const store={};
+export const store=legacy_createStore(reducer,applyMiddleware(thunk));
