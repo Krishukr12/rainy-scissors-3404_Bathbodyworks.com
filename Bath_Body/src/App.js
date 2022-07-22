69a460145d93a12cf3b775865306e546e561b5e4
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-07-22 21:22:55 +0530

    dat-4

--- Bath_Body/src/App.js
+++ Bath_Body/src/App.js
@@ -1,10 +1,11 @@
-
-
+import Checkout from "./bharat_page/Checkout.jsx";
+import Login from "./bharat_page/Login.jsx"
+import Signup from "./bharat_page/Signup"
 
 function App() {
   return (
     <div>
-      
+   <Signup/>
     </div>
   );
 }
--- Bath_Body/src/Redux/store.js
+++ Bath_Body/src/Redux/store.js
@@ -7,6 +7,9 @@ import { ArfReducer } from "./ArfReducer/reducer";
 const rootreducer=combineReducers({
     AuthReducer:AuthReducer,
     ArfReducer:ArfReducer})
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store=legacy_createStore(rootreducer,applyMiddleware(composeEnhancers(thunk)))
+
+export const store=legacy_createStore(rootreducer,applyMiddleware(thunk))
+
+
+
