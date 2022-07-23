d8f3b1508fc16553544c1e0ef835fff80c177097
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-07-23 12:35:37 +0530

    footerdone plus some updates

--- Bath_Body/src/App.js
+++ Bath_Body/src/App.js
@@ -1,13 +1,12 @@
 
 import MainRoutes from "./Routing/Routes";
-import { Navbar } from "./Components/Navbar";
-
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+ 
       <MainRoutes />
+  
     </div>
   );
 }
